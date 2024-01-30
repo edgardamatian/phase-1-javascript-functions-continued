bda4772d9e1f95cdec74e534934d891768371084
Edgar Damatian <damatian.e@gmail.com>
2024-01-30 10:47:34 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun()); // Output: "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("hike")); // Output: "This Saturday, I want to hike!"
+  console.log(saturdayFun("dance")); // Output: "This Saturday, I want to dance!"
+
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+console.log(mondayWork()); // Output: "This Monday, I will to go to the office."
+console.log(mondayWork("attend a meeting")); // Output: "This Monday, I will attend a meeting."
+console.log(mondayWork("work from home")); // Output: "This Monday, I will work from home."
+
+const wrapAdjective = function(highlight = '*') {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  };
+  
+  const adjectiveWrapper = wrapAdjective('*');
+  console.log(adjectiveWrapper('a hard worker')); // Output: "You are *a hard worker*!"
